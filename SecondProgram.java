d167b289e64255dae2c1cae5ff12ee262b75499e
rahulritesh-wipro <151699621+rahulritesh-wipro@users.noreply.github.com>
2025-02-01 16:21:31 +0530

    Day 1

---
+++ SecondProgram.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("welcome");
+		System.out.println(1 + 2);
+
+		/*
+		 * System.out.println("1+2"); System.out.println("welcome"+123);
+		 * System.out.println("welcome"+123); System.out.println("welcome"+123);
+		 * System.out.println("welcome"+123); System.out.println("welcome"+123);
+		 * System.out.println("welcome"+123);
+		 */
+
+	}
+
+}
