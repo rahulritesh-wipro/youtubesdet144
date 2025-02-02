defac1e7ddb483b64ceeaea5329c6ba33f05aec6
rahulritesh-wipro <151699621+rahulritesh-wipro@users.noreply.github.com>
2025-02-02 10:37:21 +0530

    Day50

---
+++ 50Part3CrossBrowserTestsTestBaseBaseClass.java
@@ -0,0 +1,71 @@
+package testBase;
+
+import java.time.Duration;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.apache.logging.log4j.LogManager;//log4j
+import org.apache.logging.log4j.Logger;   //log4j
+
+
+public class BaseClass {
+
+	public WebDriver driver;
+	public Logger logger;
+	
+	
+	@BeforeClass
+	@Parameters({"os", "browser"})
+	public void setup(String os, String br)
+	
+	{
+	
+		logger=LogManager.getLogger(this.getClass());//Log4j
+		
+		switch(br.toLowerCase())
+		{
+		case "chrome": driver=new ChromeDriver(); break;
+		case "edge": driver=new EdgeDriver(); break;
+		default: System.out.println("No matching browser..");
+					return;
+		}
+		
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("http://localhost/opencart/upload/index.php");
+		driver.manage().window().maximize();
+	}
+	
+	@AfterClass
+	public void tearDown()
+	{
+		driver.close();
+	}
+	
+
+	public String randomeString()
+	{
+		String generatedString=RandomStringUtils.randomAlphabetic(5);
+		return generatedString;
+	}
+	
+	public String randomeNumber()
+	{
+		String generatedString=RandomStringUtils.randomNumeric(10);
+		return generatedString;
+	}
+	
+	public String randomAlphaNumeric()
+	{
+		String str=RandomStringUtils.randomAlphabetic(3);
+		String num=RandomStringUtils.randomNumeric(3);
+		
+		return (str+"@"+num);
+	}
+}
